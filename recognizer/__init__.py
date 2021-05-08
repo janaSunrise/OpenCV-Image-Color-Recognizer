427b5c53f98911cfded49f46654d614e39251900
janaSunrise <warriordefenderz@gmail.com>
2021-05-08 11:08:53 +0530

    Get the App debugged and working.

--- recognizer/__init__.py
+++ recognizer/__init__.py
@@ -1,7 +1,45 @@
+import functools
+
 import click
+import cv2
+
+from .utils import get_color_name
+
+APP = "Image Color Recognition App"
+
+# -- Other functions --
+def handle_events(img, event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        b, g, r = img[y, x]
+        b, g, r = int(b), int(g), int(r)
+
+        cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
+
+        name, closest_name = get_color_name((r, g, b))
+        if not name:
+            name = closest_name
+
+        text = f"{closest_name} | RGB: ({r}, {g}, {b})"
+
+        cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+
+        if r + g + b >= 600:
+            cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
 
 
+# -- Commands --
 @click.command()
-@click.option("-i", "--image", 'image', required=True, type=click.Path(exists=True))
+@click.option("-i", "--image", "image", required=True, type=click.Path(exists=True))
 def recognize(image):
-    print(image)
+    img = cv2.imread(image)
+
+    cv2.namedWindow(APP)
+    cv2.setMouseCallback(APP, functools.partial(handle_events, img))
+
+    while True:
+        cv2.imshow(APP, img)
+
+        if cv2.waitKey(20) & 0xFF == 27:
+            break
+
+    cv2.destroyAllWindows()
--- recognizer/utils.py
+++ recognizer/utils.py
@@ -1,25 +1,25 @@
 import webcolors
 
 
-def closest_colour(requested_colour):
-    min_colours = {}
+def closest_color(requested_color):
+    min_colors = {}
 
-    for key, name in webcolors.css3_hex_to_names.items():
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
 
-        rd = (r_c - requested_colour[0]) ** 2
-        gd = (g_c - requested_colour[1]) ** 2
-        bd = (b_c - requested_colour[2]) ** 2
-        min_colours[(rd + gd + bd)] = name
+        rd = (r_c - requested_color[0]) ** 2
+        gd = (g_c - requested_color[1]) ** 2
+        bd = (b_c - requested_color[2]) ** 2
+        min_colors[(rd + gd + bd)] = name
 
-    return min_colours[min(min_colours.keys())]
+    return min_colors[min(min_colors.keys())]
 
 
-def get_colour_name(requested_colour):
+def get_color_name(requested_color):
     try:
-        closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
+        closest_name = actual_name = webcolors.rgb_to_name(requested_color)
     except ValueError:
-        closest_name = closest_colour(requested_colour)
+        closest_name = closest_color(requested_color)
         actual_name = None
 
     return actual_name, closest_name
